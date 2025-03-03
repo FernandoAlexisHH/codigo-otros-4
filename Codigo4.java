94d843888ae7ced7f9200e627c14eb2d4552baa1
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-03-03 17:22:50 -0600

    Agregando los comentarios descriptivos del proyecto

--- Codigo4.java
+++ Codigo4.java
@@ -1,8 +1,20 @@
 package org.generation;
 
 import java.util.Scanner;
-
+    
 public class Codigo4 {
+    //Resumen del funcionamiento:
+    /*Primero con scanner leemos la entrada de la consola que el usuario esta ingresando y lo almacenamos en cada
+    * variable.
+    * Despues creamos una variable bandera que cambiara dependiendo de si el jugador 1 gano o el jugador 2 para posteriormente
+    * imprimiremos su valor.
+    * Despues hice un if para verificar la primera condicion si ambos eligieron la misma opcion se imprime empate
+    * y despues si no entra en el else donde aplico un switch dependiendo de que opcion eligio el usuario
+    * hago los if para comparar las combinaciones ganadoras en caso de cada jugador.
+    * en el caso default del switch si no introduce opciones validas el jugador 1 imprime que introduzca una valida
+    * y se sale con break
+    * al ultimo imprimo al ganador y cierro el scanner.
+    * */
     public static void main(String[] args) { //Coloque el metodo main para que corra el programa por que no lo tenía
         Scanner s = new Scanner(System.in); 
         //Solo coloque un metodo scanner nos funciona para tener ambos strings de los jugadores
